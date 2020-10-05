28d1d31a18403810c69efb4c1d99a3596feded28
“Judit” <juditcava@hotmail.com>
2020-10-05 19:45:42 +0200

    Creando estructura del proyecto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forms</h2>
+
+<form action="" method="POST">
+  <label for="email">Email:</label><br>
+  <input type="email" id="email" name="email" value=""><br>
+  <label for="psw">Password:</label><br>
+  <input type="password" id="psw" name="psw" value=""><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
